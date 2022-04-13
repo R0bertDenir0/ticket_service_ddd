b84dc281c65e363957cb39e485db381939def5fc
luka michelson <luka.michelson@gmail.com>
2022-04-13 17:01:43 +0200

    model customer

---
+++ customer/domain/customer.domain.js
@@ -0,0 +1,15 @@
+
+class customer {
+    Id;
+    FirstName;
+    LastName;
+    Email;
+
+    constructor(Id, Firstname, Lastname, Email) {
+        this.Id = Id;
+        this.FirstName = Firstname;
+        this.LastName = Lastname;
+        this.Email = Email;
+        this.Password = Password;
+    }
+}
